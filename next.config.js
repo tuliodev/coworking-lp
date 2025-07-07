d19dbb82da1b0daaa3167f42b52db5cac2c1c3ce
Tulio Resende <resendetulio51@gmail.com>
2025-07-07 07:48:23 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     // Otimização de imagens
     formats: ['image/webp', 'image/avif'],
@@ -16,4 +15,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
